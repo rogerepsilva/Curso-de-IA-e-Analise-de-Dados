1af0deabb196f08d50af143ebad12d42f5f6409c
rogerepsilva <rogerepsilva@gmail.com>
2026-07-03 20:42:12 -0300

    adicionando aulas de dados - pandas, numpy e seaborn

---
+++ app.py
@@ -0,0 +1,9 @@
+#Comando para exibir informações na tela.
+print("Boa noite!")
+
+idade = 30
+idade_dois = "30"
+idade_tres = 30.5
+
+#Type retorna o Tipo da variável.
+print(f"{type(idade)}\n{type(idade_dois)}\n{type(idade_tres)}")
